a660b0ac2eb198e1e26d164482b8d4135bc30085
tris <tris@31afb1ed-1a29-4690-bd19-64820c24822f>
2014-04-02 20:39:34 +0000

    Added new cos.ucr test file.

--- src/version.h
+++ src/version.h
@@ -22,7 +22,7 @@
 #ifndef UCHROMA_VERSION_H
 #define UCHROMA_VERSION_H
 
-#define UCHROMAREVISION "56"
+#define UCHROMAREVISION "57"
 #define UCHROMADATE "Wed 02 Apr 2014 - 21:44"
 #define UCHROMAURL "http://www.projectaten.net/uchroma"
 
